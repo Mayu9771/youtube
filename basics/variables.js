0051db932dc7f3367ff39bf7753c118e5067d578
Mayu9771 <109643884+Mayu9771@users.noreply.github.com>
2024-08-30 05:28:00 +0000

    variables

---
+++ basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "mayank@gmail.com"
+var accountPassword = 9771
+accountCity = "patna"
+let accountState;
+//accountId = 2 // not allowed
+
+accountEmail = "raj@er.com"
+accountPassword = "57635"
+accountCity ="delhi"
+
+console.log(accountId); 
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
